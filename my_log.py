8400cb135d1b9c6cc61509316f2e347c2e979ecc
Prajwal-031 <mpprajwal11@gmail.com>
2025-03-15 23:38:37 +0530

    file of register

--- my_log.py
+++ my_log.py
@@ -1,2 +1,5 @@
 Line 1: This is line 1 added at 2025-03-15 23:37:26
 Line 2: This is line 2 added at 2025-03-15 23:37:31
+Line 1: This is line 1 added at 2025-03-15 23:38:17
+Line 2: This is line 2 added at 2025-03-15 23:38:22
+Line 1: This is line 1 added at 2025-03-15 23:38:23
