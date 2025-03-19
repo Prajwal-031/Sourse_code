23c70bb951983a0d62face733b96fae00e3f67af
Prajwal-031 <mpprajwal11@gmail.com>
2025-03-19 23:11:46 +0530

    file of register is updated

--- addnewline.py
+++ addnewline.py
@@ -1,8 +1,13 @@
 import time
+import subprocess
 
 def add_line_to_file(filename, line_number):
     with open(filename, "a") as f:
         f.write(f"Line {line_number}: This is line {line_number} added at {time.strftime('%Y-%m-%d %H:%M:%S')}\n")
+
+def git_commit():
+    subprocess.run(["git", "add", "."])
+    subprocess.run(["git", "commit", "-m", "file of register is updated"])
 
 # Example usage
 filename = "my_log.txt"
@@ -11,4 +16,5 @@ line_count = 0
 while True:
     line_count += 1
     add_line_to_file(filename, line_count)
-    time.sleep(5) # Add a delay to see the effect
+    git_commit()
+    time.sleep(5) # Add a delay to see the effect
